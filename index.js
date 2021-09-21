f922cabd7d79662f9d4b8da6724fd8a57a33d4a5
andr92h1 <andr92h1@edu.easj.dk>
2021-09-21 10:21:11 +0200

    house/intro

---
+++ index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+import houseData from './data/houseData';
+import house from './data/houseData';
+
+export default class HouseTourVR extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text style={styles.greeting}>
+            Room Selection
+          </Text>
+          <Text>
+           {house.House.roomName}
+          </Text>
+        </View>
+
+
+        <View style={styles.greetingBox}>
+          <Text style={styles.greeting}>
+            Room Info
+          </Text>
+          <Text>
+            {house.House.info}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  },
+});
+
+AppRegistry.registerComponent('HouseTourVR', () => HouseTourVR);
